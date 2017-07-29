69281f672ab0f4b96aa5f6347375c4b69e49a76d
Shivang Gupta <shivang.gupta@zs.com>
2017-07-29 16:15:37 +0530

    WPF application V 0.1

---
+++ userView/userView/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+
+namespace userView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Open the file              
+            var stream = File.OpenText("..\\..\\..\\..\\userView.json");
+            //Read the file              
+            string st = stream.ReadToEnd();
+            var Jresult = JArray.Parse(st);
+            var tCount = Jresult.Count;
+
+            for (int i=0;i<tCount;i++)
+           {
+                Userview uv = new Userview();
+
+                uv = JsonConvert.DeserializeObject<Userview>(Jresult[i].ToString());
+
+                StackPanel newpnl = new StackPanel();
+                newpnl.Background = new SolidColorBrush(Colors.Lavender);
+                newpnl.Margin = new Thickness(2, 4, 2, 4);
+
+                //BROWSER
+                Label lbrowser = new Label();
+                lbrowser.Content = uv.browserName;
+
+                ////URL
+                //TextBlock turl = new TextBlock();
+                //Hyperlink hurl = new Hyperlink();
+                //hurl.NavigateUri = new Uri(uv.Url);
+                //hurl.RequestNavigate += (sender, e) =>
+                //{
+                //    Process.Start(uv.browserName,e.Uri.ToString());
+                //};
+                //hurl.Inlines.Add("URL" + (i+1));
+                //turl.Inlines.Add(hurl);
+
+                //LOGO
+                Label ll = new Label();
+                ll.Content = uv.logo;
+                Image llogo = new Image();
+                llogo.Height = 90;
+                llogo.Width = 300;
+                BitmapImage myBitmapImage = new BitmapImage(new Uri(uv.logo));
+                myBitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                llogo.Source = myBitmapImage;
+
+
+                newpnl.Children.Add(llogo);
+                newpnl.Children.Add(lbrowser);
+                newpnl.Children.Add(ll);
+                
+                newpnl.PreviewMouseLeftButtonDown += (sender, e) =>
+                {
+                    Process.Start(uv.browserName, uv.Url);
+                };
+
+                StackPanel.Children.Add(newpnl);
+               
+           }
+        }
+    }
+}
---
+++ userView/userView/Userview.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace userView
+{
+    class Userview
+    {
+        public int id { get; set; }
+        public string logo { get; set; }
+        public string browserName { get; set; }
+        public string Url { get; set; }
+
+    }
+}
